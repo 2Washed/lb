72377e149d628f226d9b05350b83c54dc3d4c80b
2Washed <ababdelwahed112@gmail.com>
2026-06-11 19:29:49 +0100

    refactor(main): extract handler function

--- main.go
+++ main.go
@@ -7,40 +7,40 @@ import (
 )
 
 func main() {
-	var handler = func(w http.ResponseWriter, req *http.Request) {
-		//Clone request
-		outReq := req.Clone(req.Context())
-		outReq.URL.Scheme = "http"
-		outReq.URL.Host = "127.0.0.1:9000"
-		outReq.RequestURI = ""
+	http.HandleFunc("/", requestHandler)
+	http.ListenAndServe(":8080", nil)
+}
 
-		//Forward the request
-		res, forwardErr := http.DefaultClient.Do(outReq)
-		if forwardErr != nil {
-			log.Printf("[ERROR] Forwarding request failed, Error: %v\n", forwardErr)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write(nil)
-			return
-		}
-		defer res.Body.Close()
+func requestHandler(w http.ResponseWriter, req *http.Request) {
+	//Clone request
+	outReq := req.Clone(req.Context())
+	outReq.URL.Scheme = "http"
+	outReq.URL.Host = "127.0.0.1:9000"
+	outReq.RequestURI = ""
 
-		//Write headers
-		for headerName, headerValues := range res.Header {
-			for _, v := range headerValues {
-				w.Header().Add(headerName, v)
-			}
-		}
-		w.WriteHeader(res.StatusCode)
+	//Forward the request
+	res, forwardErr := http.DefaultClient.Do(outReq)
+	if forwardErr != nil {
+		log.Printf("[ERROR] Forwarding request failed, Error: %v\n", forwardErr)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+	defer res.Body.Close()
 
-		_, copyErr := io.Copy(w, res.Body)
-		if copyErr != nil {
-			log.Printf("[ERROR] Copying request to client failed, Error: %v\n", copyErr)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write(nil)
-			return
+	//Write headers
+	for headerName, headerValues := range res.Header {
+		for _, v := range headerValues {
+			w.Header().Add(headerName, v)
 		}
 	}
+	w.WriteHeader(res.StatusCode)
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	_, copyErr := io.Copy(w, res.Body)
+	if copyErr != nil {
+		log.Printf("[ERROR] Copying request to client failed, Error: %v\n", copyErr)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
 }
